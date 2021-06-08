d584c83664ad56fa76b9f2f4faad505bba1f3ae5
KentaroKOMATSU <ke-komatsu@datatech.co.jp>
2021-06-08 11:40:23 +0900

    デリゲートとプロトコル

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 利用可能なセルを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
+        // 値を設定する
+        cell.textLabel!.text = "Row\(indexPath.row)"
+        
+        return cell
+    }
 }
 
