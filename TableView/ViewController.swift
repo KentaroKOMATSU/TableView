662137fbaba02417c7ec78af2818091cc70d5196
KentaroKOMATSU <ke-komatsu@datatech.co.jp>
2021-06-08 10:50:41 +0900

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by P01575 on 2021/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
